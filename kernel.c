c19f50b1c430f0facaa631c9c42a7d0b65c93e7b
Jacob Seadorf <bloggingpenguin@gmail.com>
2021-08-21 13:19:57 -0400

    Added linker script & error checking to kernel.c

--- kernel.c
+++ kernel.c
@@ -2,6 +2,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#if defined(__linux__)
+#error "No cross compiler detected!"
+#endif
+
+#if !defined(__i386__)
+#error "This needs to be compiled with a ix86-elf compiler!"
+#endif
+
 enum vga_color {
   VGA_COLOR_BLACK = 0,
   VGA_COLOR_BLUE = 1,
